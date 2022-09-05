08cf0c30578aebc19fe0585a889b4d619ab5190f
vvarona <vvaronad@gmail.com>
2022-09-05 22:34:41 +0200

    return movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const data = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -16,20 +17,7 @@ server.listen(serverPort, () => {
 server.get('/movies', (req, res) => {
   const response = {
       success: true,
-      movies: [
-        {
-          id: '1',
-          title: 'Gambita de dama',
-          gender: 'Drama',
-          image: 'https://via.placeholder.com/150'
-        },
-        {
-          id: '2',
-          title: 'Friends',
-          gender: 'Comedia',
-          image: 'https://via.placeholder.com/150'
-        }
-      ]
+      movies: data
     };
     res.json(response);
   });
