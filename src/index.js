be3740b6366de1c4c3d2169eb3659c60387e8807
vvarona <vvaronad@gmail.com>
2022-09-05 20:21:34 +0200

    create get movie endpoint

--- src/index.js
+++ src/index.js
@@ -11,3 +11,25 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+
+server.get('/movies', (req, res) => {
+  const response = {
+      success: true,
+      movies: [
+        {
+          id: '1',
+          title: 'Gambita de dama',
+          gender: 'Drama',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: '2',
+          title: 'Friends',
+          gender: 'Comedia',
+          image: 'https://via.placeholder.com/150'
+        }
+      ]
+    };
+    res.json(response);
+  });
--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
@@ -10,3 +11,4 @@ ReactDOM.render(
   </HashRouter>,
   document.querySelector('#root')
 );
+
