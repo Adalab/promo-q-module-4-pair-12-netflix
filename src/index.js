4a2c4dcc1fa94146e42243556bb509f396664717
vvarona <vvaronad@gmail.com>
2022-09-09 11:24:05 +0200

    add user login and email verification

--- src/index.js
+++ src/index.js
@@ -28,39 +28,39 @@ server.get('/movies/', (req, res) => {
   // ejecutamos la query
   const response = query.all();
   console.log(response);
-  res.json(response)
+  res.json({ success: true, movies: response })
 });
 
 
 server.post('/login', (req, res) => {
-  {
-    console.log('me llaman ');
-    console.log(req.body);
 
-    // preparamos la query
-    const query = db.prepare(
-      'SELECT * FROM users WHERE email = ? AND password = ?'
-    );
-    console.log(query);
-    const response = query.get(req.body.email, req.body.password);
-    console.log(response);
+  // preparamos la query
+  const query = db.prepare(
+    'SELECT * FROM users WHERE email = ? AND password = ?'
+  );
+  console.log(query);
+  const user = query.get(req.body.email, req.body.password);
+  res.json({
+    "success": true,
+    "userId": user.id
+  })
 
-/*     if (query.find(user => user.email === req.body.email && user.password === req.body.password)) {
-      console.log('si esta email');
-      res.json({
-        "success": true,
-        "userId": "id_de_la_usuaria_encontrada"
-      })
-    } else {
-      console.log('no esta bien');
-      res.json({
-        "success": false,
-        "errorMessage": "Usuaria/o no encontrada/o"
-      })
-    } */
-  }
+  /*     if (query.find(user => user.email === req.body.email && user.password === req.body.password)) {
+        console.log('si esta email');
+        res.json({
+          "success": true,
+          "userId": "id_de_la_usuaria_encontrada"
+        })
+      } else {
+        console.log('no esta bien');
+        res.json({
+          "success": false,
+          "errorMessage": "Usuaria/o no encontrada/o"
+        })
+      } */
+}
 
-});
+);
 
 
 server.get('/movie/:movieId', (req, res) => {
@@ -80,6 +80,29 @@ server.get('/movie/:movieId', (req, res) => {
 
 });
 
+server.post('/sign-up', (req, res) => {
+  const emailVerify = db.prepare(
+    'SELECT * FROM users WHERE email = ?'
+  )
+  const user = emailVerify.get(req.body.email);
+  console.log(user);
+  if (user !== "") {
+    console.log('ya esta registrada');
+  } else {
+    const query = db.prepare(
+      'INSERT INTO users (email, password) VALUES (? , ? )'
+    );
+    const newUser = query.run(req.body.email, req.body.password);
+
+    res.json({
+      success: true,
+      userId: newUser.id,
+      data: []
+    })
+  }
+  
+})
+
 
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
@@ -87,7 +110,8 @@ server.use(express.static(staticServer));
 const staticImagesServer = './src/public-movies-images';
 server.use(express.static(staticImagesServer));
 
-
+/* 
 server.get('*', (req, res) => {
   res.send('<p>No hay nada aquí</p>')
 })
+ */
